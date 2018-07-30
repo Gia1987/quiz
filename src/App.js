9e5e5bbb6fe8fdd0cd20577747a754feb31b0365
Gia1987 <giacomo.n27@gmail.com>
2018-07-30 12:03:11 +0100

    create state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quiz: ['Whats the color of the Sky?', 'How many day in a year?'],
+      count: 0
+    };
+  }
 
-
+  countHandler = ()=>{
+    this.setState({
+      count  : this.state.count += 1
+    })
+  }
   questions = () => {
-    let quiz = ['Whats the color of the Sky?', 'How many day in a year?'];
-    return quiz[0]
+    return this.state.quiz[this.state.count]
   }
   render() {
     return (
       <div className="App">
-        <p> {this.questions()}</p>
+        <p>{this.questions()}</p>
+        <form onClick= {this.countHandler()}>
+          <button>Next</button>
+        </form>
       </div>
     );
   }
