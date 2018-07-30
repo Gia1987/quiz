6658c7f6562d06352ad3aa60020aead3c4663c80
Gia1987 <giacomo.n27@gmail.com>
2018-07-30 11:41:00 +0100

    First question added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  questions = () => {
+    let quiz = ['Whats the color of the Sky?', 'How many day in a year?'];
+    return quiz[0]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p> {this.questions()}</p>
       </div>
     );
   }
